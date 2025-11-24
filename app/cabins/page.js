844a840091201a663ee5e6832145830b4cdd1b36
codejordy25 <jordytsimba11@gmail.com>
2025-11-24 15:12:23 +0100

    Fetch data in page and add interactivity With client Comp

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,18 @@
 import Navigation from "@/app/components/Navigation";
+import Counter from "../components/Counter";
+const res = await fetch("https://jsonplaceholder.typicode.com/users");
+const data = await res.json();
+console.log(data);
 function page() {
   return (
     <div>
       <h1>cabins pages</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
     </div>
   );
 }
